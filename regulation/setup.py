da12f89d2e1c60c76d3008771cc03d1df14f5377
Ruben <ronatvik@gmail.com>
2020-07-29 14:35:51 +0200

    Added regulation package

---
+++ regulation/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dsp-module",
+    version="0.0.1",
+    author="Ruben Natvik",
+    author_email="ronatvik@gmail.com",
+    description="A module containing useful tools for digital signal processing.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RNatvik/rntools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
