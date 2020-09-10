f923fe4800a2bd58351ab3babf23603507ceb084
Ruben <ronatvik@gmail.com>
2020-09-10 20:04:14 +0200

    Added readme, requirements and setup.py

---
+++ general/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", 'r') as req:
+    requirements = req.read().split('\n')
+
+setuptools.setup(
+    name="general",
+    version="0.0.1",
+    author="Ruben Natvik",
+    author_email="ronatvik@gmail.com",
+    description="A package containing general tools.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    url="https://github.com/RNatvik/rntools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
